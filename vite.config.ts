d99f0480939bc6f46c9bf0b24b16ec3736fcaae5
repunto <repunto@gmail.com>
2026-07-09 12:20:51 -0500

    CI/CD: Add GitHub Actions workflow for automatic Vite build and Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/inmobiliaria-tours-virtuales-repunto/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
